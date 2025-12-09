b87de6d8c3530f813e53ca3cab5ee787f85c8ae5
Rashed <rashedghanem1162@gmail.com>
2025-12-09 21:57:34 +0300

    change launch settings

--- Ajeer.mobile/lib/config/app_config.dart
+++ Ajeer.mobile/lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-
+//192.168.100.20
   static const String baseUrl = 'http://localhost:5289';
   static const String apiUrl = '$baseUrl/api';
   static const String hubUrl = '$baseUrl/hubs/chat';
