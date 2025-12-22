ff70e5a470f9b20c83ec7f22f7eabe04979f0160
hothaifah22 <hothaifahmaen45@gmail.com>
2025-12-22 21:30:05 +0300

    Refactor code structure for improved readability and maintainability

--- Ajeer.mobile/lib/config/app_config.dart
+++ Ajeer.mobile/lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  //192.168.100.20 //http://localhost:5289'; //192.168.1.46 //192.168.1.49
+  //192.168.100.20 //http://localhost:5289'; //192.168.1.49 //172.20.10.5
   static const String baseUrl = 'http://192.168.1.49:5289';
   static const String apiUrl = '$baseUrl/api';
   static const String hubUrl = '$baseUrl/hubs/chat';
